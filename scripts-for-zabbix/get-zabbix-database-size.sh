a598fd235e1a1e699e391883c7ebe55785999392
Kean.Lee <haoli201410@163.com>
2016-10-10 09:35:24 +0800

    Update get-zabbix-database-size.sh

--- scripts-for-zabbix/get-zabbix-database-size.sh
+++ scripts-for-zabbix/get-zabbix-database-size.sh
@@ -6,4 +6,4 @@
 
 
 databases_zabbix=$(mysql -uzabbix -pzabbix -e "show databases;use information_schema;select concat(round(sum(DATA_LENGTH/1024/1024),2),'MB') as data from TABLES where table_schema='zabbix';" )
-echo $databases_zabbix | awk  '{print $6}'
+echo $databases_zabbix | awk  '{print $5}'
