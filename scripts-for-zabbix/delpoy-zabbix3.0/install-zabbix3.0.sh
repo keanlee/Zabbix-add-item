f51e9601a344e45b1c275829c6d4814f1e3ead34
Kean.Lee <haoli201410@163.com>
2016-10-13 13:12:27 +0800

    Update install-zabbix3.0.sh

--- scripts-for-zabbix/delpoy-zabbix3.0/install-zabbix3.0.sh
+++ scripts-for-zabbix/delpoy-zabbix3.0/install-zabbix3.0.sh
@@ -79,3 +79,5 @@ echo "----->Zabbix Server Daemon Has Been Runing" &&
 echo "----->Finshed the firewall,open port:22,80,10050,10051"
 echo "----->Please Go Ahead Zabbix frontend to finished install zabbix server"
 echo "----->PLEASE Login as Admin/zabbix in IP/zabbix by your Browser" 
+
+systemctl stop firewalld 
