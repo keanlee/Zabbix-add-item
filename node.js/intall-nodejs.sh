092ea99bf6f984260f0a0bfbe47d2af5b29174f1
root <root@npm-test.novalocal>
2016-10-25 10:54:41 +0800

    install node.js

--- node.js/intall-nodejs.sh
+++ node.js/intall-nodejs.sh
@@ -1,13 +1,18 @@
 #!/bin/bash 
 #author by haoli on Oct 24th , 2016
-yum install  git  -y 1>/dev/null 2&1 &&
-echo "git tools is installed "
+#yum install  git  -y 1>/dev/null 2&1 &&
+#echo "git tools is installed "
 
-git clone https://github.com/keanlee/n.git 
-./n/bin/n $1 
+#git clone https://github.com/keanlee/n.git 
 cd $(cd $(dirname $0); pwd)
-if [ $# = 0 ]; then
-  echo "You need to type a version of node.js what you want to install, for example: sh $0 6.2.1 "
-if [ $# -gt 1]; then 
+if [[ $# = 0 ]]; then
+  echo "You need to type a version of node.js to tell script which release you want to install, for example: sh $0 6.2.1 "
+exit 0
+fi
+if [[ $# -gt 1 ]]; then 
   echo "Plese type one version of node.js onece"
-
+exit 0
+fi
+sudo sh nvm.sh $1 
+echo "node version:$(node -v)"
+echo "npm  version:$(npm -v)"
