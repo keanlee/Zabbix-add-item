7beaa656d73f0ac0ed4e4b87c2383e80f1cfc314
root <root@redhat.novalocal>
2016-12-02 11:09:32 +0800

    add

--- scripts-for-zabbix/get-zabbix-database-size.sh
+++ scripts-for-zabbix/get-zabbix-database-size.sh
@@ -3,4 +3,4 @@
 databases_zabbix=$(mysql -uzabbix -pzabbix -e "show databases;use information_schema;select concat(round(sum(DATA_LENGTH/1024/1024),2),'MB') as data from TABLES where table_schema='zabbix';" )
 echo "              " >> zabbix-database-size.txt
 echo $(date)":" >> zabbix-database-size.txt
-echo $databases_zabbix  | awk '{print $5}' >> zabbix-database-size.txt
+echo $databases_zabbix  | awk '{print $4}' >> zabbix-database-size.txt
