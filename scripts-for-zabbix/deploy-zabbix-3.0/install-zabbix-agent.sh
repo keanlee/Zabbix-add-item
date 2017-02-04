ca0146a4f7812abd478b604e287299068c6be286
keanlee <haoli201410@163.com>
2017-02-04 21:04:30 +0800

    install-zabbix-agent add

--- scripts-for-zabbix/deploy-zabbix-3.0/install-zabbix-agent.sh
+++ scripts-for-zabbix/deploy-zabbix-3.0/install-zabbix-agent.sh
@@ -38,20 +38,25 @@ echo "zabbix-agent installed"
 yum install zabbix-sender -y  1>/dev/null 2>&1 &&
 echo "zabbix-sender installed "
 function config(){
-sed -i 's/Server=127.0.0.1/Server=$1/g' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/ServerActive=127.0.0.1/ServerActive=$1/g'  /etc/zabbix/zabbix_agentd.conf
-sed -i 's/Hostname=Zabbix\ server/Hostname=$2/g'  /etc/zabbix/zabbix_agentd.conf
-sed -i '167 i HostMetadata=$3' /etc/zabbix/zabbix_agentd.conf
+sed -i "s/Server=127.0.0.1/Server=$1/g" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/ServerActive=127.0.0.1/ServerActive=$1/g"  /etc/zabbix/zabbix_agentd.conf
+sed -i "s/Hostname=Zabbix\ server/Hostname=$2/g"  /etc/zabbix/zabbix_agentd.conf
+sed -i "167 i HostMetadata=$3"  /etc/zabbix/zabbix_agentd.conf
 }
-read -p  "Please type the zabbix-server ip(for example:192.168.0.1), ServerActive's ip(same as server ip),hostname,hostmetadata: " server-ip server-ip1 
-hostname hostmetdata
-config $server-ip $server-ip1 $hostname $hostmetdata &&
-echo "Has been finish the zabbix-agent conf file setup"
+#read -p  "Please type the zabbix-server ip(for example:192.168.0.1),hostname,hostmetadata: " serverip hostname hostmetdata
+#config $serverip $hostname $hostmetdata
+echo "Please type the zabbix-server's ip: "
+read zabbixserverip
+echo "Plese type the Hostname(This server ip is also ok) that will be show on the zabbix-server (click Configuration->Hosts on web page) : "
+read hostname 
+echo "Please type the HostMetadata: "
+read metadata
+config $zabbixserverip $hostname $metadata
+#echo "Has been finish the zabbix-agent conf file setup"
 systemctl enable zabbix-agent 
 systemctl start zabbix-agent &&
-echo "Zabbix agent has been install, you can go ahead to the zabbix server to add this server to host list ,thanks you use this scrip to install zabbix "
+echo "Zabbix agent has been install, you can go ahead to the zabbix server to add this server to host list ,thank you use this scrip to install zabbix-agent "
 }
-
 
 function clean(){
       yum remove zabbix-agent zabbix-sender -y 
@@ -61,8 +66,10 @@ function clean(){
 function clean(){
       yum erase zabbix-agent zabbix-sender -y
       }
-zabbixcount=$(rpm -qa | grep zabbix | wc -l)
-if [[ $(zabbixcount) -ge 2 ]]; then
+#zabbixcount="$(rpm -qa | grep zabbix | wc -l)"
+if [ $(rpm -qa | grep zabbix | wc -l) -ge 1 ];then
 clean
-fi
 install
+else
+install
+fi
