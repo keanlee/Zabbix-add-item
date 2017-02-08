728c6cdb887969764bb591952f4c77ce13bd6889
Kean.Lee <haoli201410@163.com>
2017-02-08 11:32:43 +0800

    Update install-zabbix-server-3.0.7.sh

--- scripts-for-zabbix/deploy-zabbix-3.0/install-zabbix-server-3.0.7.sh
+++ scripts-for-zabbix/deploy-zabbix-3.0/install-zabbix-server-3.0.7.sh
@@ -37,7 +37,7 @@ EOF
       mv ./zabbixbase.repo /etc/yum.repos.d/
      
 }
- zabbix_3.0.repos(){
+ zabbix_3.0_.repos(){
             echo > zabbix3.0.repo
             cat > ./zabbix3.0.repo << EOF
 [zabbix]
@@ -66,7 +66,7 @@ function choiceversion(){
        ;; 
        3.0)
         yum_zabbixbase_repo_install 
-        zabbix_3.0.repos
+        zabbix_3.0_.repos
        esac 
      }
 #echo "Please choose which version of zabbix-server you want to install (3.0 or 3.2): "  
