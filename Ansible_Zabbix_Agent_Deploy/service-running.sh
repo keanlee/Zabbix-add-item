694815589aca1c4d681d08d355ccd4d80ceeb235
Kean.Lee <haoli201410@163.com>
2017-02-21 10:00:34 +0800

    Create service-running.sh

---
+++ Ansible_Zabbix_Agent_Deploy/service-running.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+#author by lihao on Feb 20th
+daemon_status=$(systemctl status $1 | grep Active | awk -F ":" '{print $2}' | awk '{print $1}')
+
+if [[ $daemon_status == "inactive" ]]; then
+echo "0" #daemon is running 
+else
+echo "1" #daemon is down
