810252c0cfeb3d8e7aac1a2555f74f67046ed358
Kean.Lee <haoli201410@163.com>
2017-03-19 16:49:46 +0800

    Update and rename deamon-status.sh to serviceexist.sh

--- scripts-for-zabbix/deploy-zabbix-3/deploy-agent/script/common/serviceexist.sh
+++ scripts-for-zabbix/deploy-zabbix-3/deploy-agent/script/common/serviceexist.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 #author by lihao on Feb 20th
+#this script can be display deamon which is you type if running 
 daemon_status=$(systemctl status $1 | grep Active | awk -F ":" '{print $2}' | awk '{print $1}')
 if [[ $daemon_status == "inactive" ]]; then
 echo "0" #daemon is running 
