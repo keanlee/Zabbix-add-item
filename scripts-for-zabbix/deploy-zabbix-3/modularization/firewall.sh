674f19cae7613ee81d31cd1693cb5499b810c2c6
Kean.Lee <haoli201410@163.com>
2017-03-23 21:31:25 +0800

    Update firewall.sh

--- scripts-for-zabbix/deploy-zabbix-3/modularization/firewall.sh
+++ scripts-for-zabbix/deploy-zabbix-3/modularization/firewall.sh
@@ -8,6 +8,7 @@ sed -i "60 i -A INPUT -p tcp -m multiport --ports 22 -m comment --comment \"sshd
 sed -i "61 i -A INPUT -p tcp -m multiport --ports 10050 -m comment --comment \"zabbix server \" -j ACCEPT " /etc/sysconfig/iptables
 sed -i "62 i -A INPUT -p tcp -m multiport --ports 10051 -m comment --comment \"zabbix server \" -j ACCEPT " /etc/sysconfig/iptables
 sed -i "63 i -A INPUT -p tcp -m multiport --ports 80 -m comment --comment \"httpd \" -j ACCEPT " /etc/sysconfig/iptables
+iptables-save >/etc/sysconfig/iptaables
 systemctl restart iptables
 }
 
@@ -21,9 +22,10 @@ firewall-cmd --reload  1>/dev/null 2>&1
 
 if [[ -d /etc/firewalld ]];then
 firewalld
-else
+elif [ -f /etc/sysconfig/iptaables ];then 
 iptabels
+else 
+continue 
 fi
-
 echo -e "\e[1;32m Congratulation !!! You has been finished zabbix $(rpm -qa | grep zabbix-web-mysql | awk -F "-" '{print $4}') install \e[0m"
                         
