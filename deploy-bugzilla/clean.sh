31432fbb1f2b641c8e227e41575832599682ee40
Kean.Lee <haoli201410@163.com>
2017-03-23 13:34:42 +0800

    Update clean.sh

--- deploy-bugzilla/clean.sh
+++ deploy-bugzilla/clean.sh
@@ -1,6 +1,12 @@
 #!/bib/bash 
 #author by lihao
-echo "Begin to clean env........"
+# ansi colors for formatting heredoc
+ESC=$(printf "\e")
+GREEN="$ESC[0;32m"
+NO_COLOR="$ESC[0;0m"
+RED="$ESC[0;31m"
+
+echo $RED Begin to clean env........ $NO_COLOR
 yum erase -y  mariadb-server mariadb mariadb-libs 1>/dev/null 2>&1
 yum erase -y  httpd httpd-tools 1>/dev/null 2>&1
 rm -rf /var/lib/mysql
@@ -10,5 +16,5 @@ rm -f /etc/yum.repos.d/*
 yum clean all   1>/dev/null 2>&1
 rm -rf /etc/httpd
 rm -rf /var/www/html/
-echo "Finished clean Env"
+echo $GREEN Finished clean Env $NO_COLOR
 
