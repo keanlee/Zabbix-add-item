d8f54afc2c44bf519a7b3388ef81a77e315b97b9
Kean.Lee <haoli201410@163.com>
2017-03-23 10:05:51 +0800

    Update serviceexist.sh

--- scripts-for-zabbix/deploy-zabbix-3/deploy-agent/script/common/serviceexist.sh
+++ scripts-for-zabbix/deploy-zabbix-3/deploy-agent/script/common/serviceexist.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 #author by lihao on Feb 20th
 #this script can be display deamon which is you type if running 
-daemon_status=$(systemctl status $1 | grep Active | awk -F ":" '{print $2}' | awk '{print $1}')
-if [[ $daemon_status == "inactive" ]]; then
+STATUS=$(systemctl status $1 | grep Active | awk -F ":" '{print $2}' | awk '{print $1}')
+if [[ $STATUS == "inactive" ]]; then
 echo "0" #daemon is running 
 else
 echo "1" #daemon is down
