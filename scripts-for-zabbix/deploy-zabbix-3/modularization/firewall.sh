da7531cf1dad1cd2a1972413b98061f1c1109bfe
Kean.Lee <haoli201410@163.com>
2017-03-28 11:53:21 +0800

    Update firewall.sh

--- scripts-for-zabbix/deploy-zabbix-3/modularization/firewall.sh
+++ scripts-for-zabbix/deploy-zabbix-3/modularization/firewall.sh
@@ -1,15 +1,17 @@
 #!/bin/bash 
 #-------------------this script can be help you configuer the firewall rule for zabbix server ------
 #author by lihao in March of 2017
+STATUS=$(systemctl status firewalld | grep Active | awk -F ":" '{print $2}' | awk '{print $1}')
 
 function iptabels(){
-sed -i "60 i -A INPUT -p tcp -m multiport --ports 22 -m comment --comment \"sshd \" -j ACCEPT " /etc/sysconfig/iptables
 #iptables -P INPUT DROP
-sed -i "61 i -A INPUT -p tcp -m multiport --ports 10050 -m comment --comment \"zabbix server \" -j ACCEPT " /etc/sysconfig/iptables
-sed -i "62 i -A INPUT -p tcp -m multiport --ports 10051 -m comment --comment \"zabbix server \" -j ACCEPT " /etc/sysconfig/iptables
-sed -i "63 i -A INPUT -p tcp -m multiport --ports 80 -m comment --comment \"httpd \" -j ACCEPT " /etc/sysconfig/iptables
+iptables -I  INPUT -p tcp --dport 22    -j ACCEPT
+#iptables -P INPUT DROP
+iptables -A  INPUT -p tcp --dport 80    -j ACCEPT
+iptables -A  INPUT -p tcp --dport 10050 -j ACCEPT
+iptables -A  INPUT -p tcp --dport 10051 -j ACCEPT
 iptables-save >/etc/sysconfig/iptaables
-systemctl restart iptables
+#systemctl restart iptables
 }
 
 function firewalld(){
@@ -20,12 +22,11 @@ firewall-cmd --zone=public --add-port=10051/tcp --permanent 1>/dev/null 2>&1
 firewall-cmd --reload  1>/dev/null 2>&1
 }
 
-if [[ -d /etc/firewalld ]];then
+if [[ $STATUS = active ]];then
 firewalld
-elif [ -f /etc/sysconfig/iptaables ];then 
+elif [ -f /etc/sysconfig/iptaables ];then
 iptabels
-else 
-continue 
+else
+continue
 fi
 echo -e "\e[1;32m Congratulation !!! You has been finished zabbix $(rpm -qa | grep zabbix-web-mysql | awk -F "-" '{print $4}') install \e[0m"
-                        
