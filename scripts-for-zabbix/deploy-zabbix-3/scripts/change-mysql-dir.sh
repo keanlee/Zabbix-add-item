1dde2745ff320852e9e8643958e54cd9b5ab80bf
Kean.Lee <haoli201410@163.com>
2017-03-31 16:23:04 +0800

    Create change-mysql-dir.sh

---
+++ scripts-for-zabbix/deploy-zabbix-3/scripts/change-mysql-dir.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+systemctl stop mariadb &&
+
+#format the disk 
+mkfs.ext4 /dev/vdb      #replace by your know partition  e.g. :vda vdb sdb etc  
+mount /dev/vdb /home
+
+cp -R /var/lib/mysql/  /home/
+chown -R mysql:mysql /home/mysql
+#edit my.cnf
+sed -i 's/datadir=\/var\/lib\/mysql/datadir=\/home\/mysql' /etc/my.cnf
+sed -i 's/socket=\/var\/lib\/mysql\/mysql.sock/socket=\/home\/mysql\/mysql.sock' /etc/my.cnf
+systemctl start mariadb &&
+ln -s /home/mysql/mysql.sock /var/lib/mysql/mysql.sock
+echo "change dir of mysql done "
